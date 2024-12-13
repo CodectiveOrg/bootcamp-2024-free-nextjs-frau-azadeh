5e3181c5f925a04d757f9d1f0f81f681f4157496
Azadeh Sharifi Soltani <68397017+frau-azadeh@users.noreply.github.com>
2024-12-13 06:30:32 -0800

    Lesson 1 (#2)
    * delete READM.md
    
    * install next.js14 and typescript with app router
    
    * change page.tsx
    
    * delete css page.module.css
    
    * insert vazir font
    
    * change my .gitignore
    
    * font Geist delete
    
    * change my README.md

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from 'next/font/google'
+import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin','arabic'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "کلینیک فوق تخصصی دیابت تابان",
+  description: "کلینیک فوق تخصصی دیابت تابان در شهرک غرب تهران همراه با پزشکان مجرب آماده پاسخگویی به هموطنان می باشند",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>کلینیک فوق تخصصی دیابت تابان</h1>
+    </div>
+  );
+}
