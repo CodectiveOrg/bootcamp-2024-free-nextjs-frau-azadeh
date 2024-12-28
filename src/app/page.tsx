8b4cc8cd06d637792810d67bc81b94bad93aa5ec
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2024-12-28 06:28:25 -0800

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { ReactElement } from "react";
-
 import GlobalSearchBoxComponent from "@/components/global-search-box/GlobalSearchBoxComponent";
-
 import MyDoctorLogo from "@/logo/my-doctor-logo";
-
 import styles from "./page.module.css";
 
 export default function Home(): ReactElement {
