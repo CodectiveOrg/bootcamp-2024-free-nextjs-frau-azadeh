35d388a081c49d813c88d4eeca7880e73cb6560f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-04 08:35:59 -0800

    install react icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,34 @@ import { ReactElement } from "react";
 import GlobalSearchBoxComponent from "@/components/global-search-box/GlobalSearchBoxComponent";
 import MyDoctorLogo from "@/logo/my-doctor-logo";
 import styles from "./page.module.css";
+import SliderComponent from "@/components/slider/SliderComponent";
+import FAQsAndReviews from "@/components/faq/FAQsAndReviews";
+import ReviewsSlider from "@/components/review/ReviewsSlider";
+import GuideSteps from "@/components/guide/GuideSteps";
+import { Stats } from "fs";
+import StatsCards from "@/components/stats/StatsCards";
 
 export default function Home(): ReactElement {
   return (
     <div className={styles.home}>
+      <SliderComponent/>
+      <br></br>
       <h1>
         <MyDoctorLogo />
         دکتر من
       </h1>
       <GlobalSearchBoxComponent />
       <div className={styles.history}>
-        <div className={styles.title}>آخرین جستجوهای شما</div>
-        <ul>
-          <li>ارتوپد</li>
-          <li>قلب و عروق</li>
-        </ul>
+      </div>
+      <br></br>
+      <GuideSteps/>
+      <br></br>
+      <StatsCards/>
+      <br></br>
+      <div className={styles.container}>
+        <FAQsAndReviews />
+        <ReviewsSlider />
+        <br></br>
       </div>
     </div>
   );
---
+++ src/components/guide/GuideSteps.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import styles from './guideSteps.module.css';
+
+const GuideSteps: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.guide}>راهنمای بخش اورژانس</div>
+        <div className={styles.guide}>مسیرهای منتهی به بیمارستان</div>
+        <div className={styles.guide}>راهنمای پذیرش</div>
+        <div className={styles.guide}>راهنمای طبقات</div>
+        <div className={styles.guide}>راهنمای ترخیص و دریافت مدارک</div>
+      </div>
+      <div className={styles.image}>
+        <img src="/assets/guide/doctor-guide.webp" alt="راهنمای مراجعین" />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.guide}>راهنمای ملاقات</div>
+        <div className={styles.guide}>برنامه کلینیک فوق تخصصی</div>
+        <div className={styles.guide}>راهنمای صدور گواهی ولادت</div>
+        <div className={styles.guide}>آزمایشگاه و پاتولوژی</div>
+        <div className={styles.guide}>خدمات مراقبتی</div>
+      </div>
+    </div>
+  );
+};
+
+export default GuideSteps;
