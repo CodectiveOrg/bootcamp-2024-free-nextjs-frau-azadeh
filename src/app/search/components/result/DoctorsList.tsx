7d118b5b7972269f1ad8f6c0862b3354284fd947
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-08 03:02:03 -0800

    add DoctorsList components

---
+++ src/app/search/components/result/DoctorsList.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from 'react';
+import { useFilterContext } from '../../context/FilterContext';
+import DoctorCard from './DoctorCard';
+
+const DoctorsList: React.FC = () => {
+  const { doctors, genderFilter } = useFilterContext();
+
+  // فیلتر کردن دکترها بر اساس جنسیت
+  const filteredDoctors = doctors.filter((doctor) =>
+    genderFilter ? doctor.gender === genderFilter : true
+  );
+
+  return (
+    <div>
+      {filteredDoctors.length === 0 ? (
+        <p>هیچ دکتری پیدا نشد</p>
+      ) : (
+        filteredDoctors.map((doctor) => (
+          <DoctorCard
+            key={doctor.id}
+            name={doctor.name}
+            gender={doctor.gender}
+            specialty={doctor.specialty}
+            degree={doctor.degree}
+            workTime={doctor.workTime}
+            image={doctor.image}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default DoctorsList;
