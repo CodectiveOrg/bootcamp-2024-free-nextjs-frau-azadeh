d098acf427b5f4049dc9161b7c4a7eba7fc5437a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-18 06:46:24 -0800

    delete commit

--- src/components/slider/SliderComponent.tsx
+++ src/components/slider/SliderComponent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image"; // استفاده از کامپوننت Image
+import Image from "next/image"; 
 import slider from "@/assets/slider/slider.webp";
 import slider1 from "@/assets/slider/slider-1.webp";
 import slider2 from "@/assets/slider/slider-2.webp";
@@ -10,13 +10,12 @@ import styles from "./slider.module.css";
 const SliderComponent = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // آرایه تصاویر
   const images = [slider, slider1, slider2];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // تغییر تصویر هر 5 ثانیه
+    }, 5000); 
     return () => clearInterval(interval);
   }, [images.length]);
 
@@ -28,12 +27,12 @@ const SliderComponent = () => {
           className={`${styles.slide} ${index === currentIndex ? styles.active : ""}`}
         >
           <Image
-            src={image} // استفاده از مسیر تصویر
-            alt={`Slide ${index}`} // متن جایگزین
+            src={image} 
+            alt={`Slide ${index}`} 
             className={styles["slide-image"]}
-            layout="fill" // پر کردن کانتینر
-            objectFit="cover" // مقیاس‌دهی تصویر برای پر کردن کل صفحه
-            priority // برای بارگذاری سریع‌تر تصویر اول
+            layout="fill" 
+            objectFit="cover" 
+            priority 
           />
         </div>
       ))}
