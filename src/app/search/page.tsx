20be242799a89653c51b8ff8a093ad1f6b05275e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-18 06:45:57 -0800

    update search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,13 +2,15 @@ import React, { ReactElement } from "react";
 import styles from "./search.module.css";
 import { FilterProvider } from "./context/FilterContext";
 import DoctorsList from "./components/result/DoctorsList";
-import FilterGender from "./components/filter/FilterGende";
+import FilterGender from "./components/filter/FilterGender";
+import FilterDegree from "./components/filter/FilterDegree";
 export default function page(): ReactElement {
   return (
     <FilterProvider>
       <div className={styles.container}>
         <aside className={styles.filters}>
           <FilterGender />
+          <FilterDegree />
         </aside>
         <section className={styles.result}>
           <DoctorsList />
