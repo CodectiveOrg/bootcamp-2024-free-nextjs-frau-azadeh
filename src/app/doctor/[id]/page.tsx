ee5980c7ea248645a66f933e31cd7b07efb7c044
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-21 03:54:11 -0800

    add notFound

--- src/app/doctor/[id]/page.tsx
+++ src/app/doctor/[id]/page.tsx
@@ -1,4 +1,5 @@
 import doctors from "@/data/doctors.json";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: { id: string };
@@ -6,7 +7,9 @@ type Props = {
 
 export default function Page({ params }: Props): React.ReactElement {
   const doctor = doctors.find((x) => x.id.toString() === params.id);
-
+    if (!doctor){
+        return notFound();
+    }
   return (
     <div>
       <h3>{doctor?.name}</h3>
