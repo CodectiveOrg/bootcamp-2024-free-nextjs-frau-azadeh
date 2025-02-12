6be8b72ef64f0005597cb6df62aa9c4294fc20c1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-12 05:16:40 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import dynamic from "next/dynamic";
 import GlobalSearchBoxComponent from "@/components/global-search-box/GlobalSearchBoxComponent";
 import MyDoctorLogo from "@/logo/my-doctor-logo";
 import styles from "./page.module.css";
-import SliderComponent from "@/components/slider/SliderComponent";
 import FAQsAndReviews from "@/components/faq/FAQsAndReviews";
 import ReviewsSlider from "@/components/review/ReviewsSlider";
 import GuideSteps from "@/components/guide/GuideSteps";
 import StatsCards from "@/components/stats/StatsCards";
 
-// 👇 HeroSection را داینامیک بدون SSR لود می‌کنیم
 const HeroSection = dynamic(() => import("@/components/signin/HeroSection"), {
   ssr: false,
 });
@@ -19,7 +17,6 @@ const HeroSection = dynamic(() => import("@/components/signin/HeroSection"), {
 export default function Home(): ReactElement {
   return (
     <div className={styles.home}>
-      {/* <SliderComponent /> */}
       <HeroSection />
       <br />
       <h1>
